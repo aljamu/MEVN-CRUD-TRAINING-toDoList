09172ea669e68f49b0e918db837c53a2ea8387f0
Al. Ja. M.-U. <150540760+aljamu@users.noreply.github.com>
2024-07-25 18:24:27 +0200

    Set up app.js and start server

--- backend/app.js
+++ backend/app.js
@@ -1,1 +1,36 @@
-console.log("Hello World!")
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+//create express app
+const app = express();
+
+//Handle CORS and Middleware
+app.use(function(req, res, next) {
+    //Cors stuff:
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE"); // If using .fetch and not axios
+    res.header("Access-Control-Allow-Headers", "auth-token, Origin, X-Requested-With, Content-Type, Accept");
+    next();
+    //
+})
+
+//Database
+const MongoDB_URI = "mongodb://localhost:27017/"
+mongoose.connect(MongoDB_URI).then(() => {
+    console.log("Connected to Mongodb Database")
+}).catch(err=> console.log(err))
+
+
+app.use(bodyParser.json())
+
+// routes
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+//start server
+app.listen(3000, ()=>{
+    console.log("Server listening at Port 3000")
+})
