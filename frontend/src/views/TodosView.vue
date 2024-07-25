d727e56c75e88aa6731134fbf37e70032c9d4240
Al. Ja. M.-U. <150540760+aljamu@users.noreply.github.com>
2024-07-25 19:37:22 +0200

    Adding Frontend with Vue

---
+++ frontend/src/views/TodosView.vue
@@ -0,0 +1,21 @@
+<template >
+    <div>
+        <h1>{{test.name}}</h1>
+    </div>
+</template>
+<script>
+import { reactive } from 'vue';
+
+export default {
+    setup() {
+        const test = reactive({
+            name:"Sideshow Bob"
+        })
+
+        return { test }
+    }
+}
+</script>
+<style scoped>
+    
+</style>
