d3237d2af7cdafdf0a50cd845541dc3273d3ab67
Al. Ja. M.-U. <150540760+aljamu@users.noreply.github.com>
2024-07-25 18:52:21 +0200

    More Routes
    CRUD

--- backend/routes/Todos.js
+++ backend/routes/Todos.js
@@ -3,10 +3,47 @@ const router = express.Router();
 const Todo = require('../models/Todos')
 
 //Get All Todo route
-
 router.get("/", async (req, res) => {
     const todos = await Todo.find();
     res.json(todos)
+});
+
+//Create new Todo
+router.post("/new", async (res, req) => {
+    const newTodo = new Todo(
+        // req.body //What the Vue App is sending
+        {
+            todo:"Go to Canada",
+            author: "Flanders",
+        }
+    );
+    const savedTodo = await newTodo.save()
+    res.json(savedTodo)
+});
+
+//Getter by ID
+router.get("/get/:id", async (res, req) => {
+    const todoById = await Todo.findById({ _id: req.params.id })
+    res.json(todoById)
+});
+
+
+//Detele a Todo by Id
+router.delete("/delete/:id", async (res, req) => {
+    const DeleteTodoById = await Todo.findByIdAndDelete({ _id: req.params.id })
+    res.json(DeleteTodoById)
+});
+
+//Update a Todo by Id
+router.put("/update/:id", async (res, req) => {
+    const UpdateTodoById = await Todo.updateOne(
+        // {_id : req.params.id}, { $set: req.body }
+        {
+            todo: "Skating",
+            author: "Bart"
+        }
+    )
+    res.json(UpdateTodoById)
 });
 
 module.exports = router;
